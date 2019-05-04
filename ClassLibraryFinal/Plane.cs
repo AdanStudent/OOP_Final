192954d0fe737742b0e3c73b68b16f4501691e09
adan.benitez <adan.benitez@loop.colum.edu>
2019-05-04 14:48:14 -0500

    Adding Final to repo

---
+++ ClassLibraryFinal/Plane.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibraryFinal
+{
+    public class Plane : MotorVehicle
+    {
+        public Plane()
+        {
+            this.MaxWeight = 1000;
+            this.TopSpeed = 200;
+            this.MaxDistancePerRefuel = 5000;
+        }
+    }
+}
